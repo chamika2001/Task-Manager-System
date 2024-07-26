83e8ec54bfd4498683a4f542e8919ceb393b3a53
pdgamage <147800696+pdgamage@users.noreply.github.com>
2024-07-26 09:29:41 +0530

    renamed:    src/Group_pages/Dashboard.js -> src/Group_pages/GroupDash.js

--- src/Group_pages/GroupDash.js
+++ src/Group_pages/GroupDash.js
@@ -2,10 +2,10 @@ import React from 'react';
 import MainContent from './MainContent';
 import '../styles/dashboard.css';
 
-const Dashboard = () => (
+const GroupDash = () => (
   <div className="dashboard">
     <MainContent />
   </div>
 );
 
-export default Dashboard;
+export default GroupDash;
